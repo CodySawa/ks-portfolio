142255091d1351a54aa844425c75cb58fbe1f1ad
CodySawa <kota418@gmail.com>
2022-07-21 15:26:03 -0700

    style and link navbar to components

--- ks-potfolio/src/components/nav.js
+++ ks-potfolio/src/components/nav.js
@@ -3,14 +3,14 @@ import { Navbar, Container, Nav, Button } from 'react-bootstrap'
 
 function NavB() {
   return (
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" fixed="top">
         <Container >
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className=" ms-auto">
-            <Nav.Link clashref="#about">About</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <Button onClick="window.location.href = 'https://google.com'" target='_blank'>Resume</Button>
+          <Nav className="ms-auto" >
+            <Nav.Link className="navlink" href="#about">About</Nav.Link>
+            <Nav.Link className="navlink" href="#projects">Projects</Nav.Link>
+            <Nav.Link className="navlink" href="#contact">Contact</Nav.Link>
+            <Button href='https://google.com' target='_blank' class="btn btn-primary-outline">Resume</Button>
           </Nav>
         </Container>
       </Navbar>
