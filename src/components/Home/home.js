3e8c02055d856731dc50762434df3e189547f1c2
CodySawa <kota418@gmail.com>
2022-08-23 15:53:52 -0700

    new home component style

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesDown } from '@fortawesome/free-solid-svg-icons';
+
 
 import './home.css';
 
 function Home() {
     return (
-        <div id="home">   
-            <div className="homeTextDiv">
+        <section id="home" className="stars-container">   
+            <div className="homeTextDiv ">
+                <p className="greeting">Hey, my name is</p>
                 <h2>Kota Sawada</h2>
-                <h2>Kota Sawada</h2>
-                <p>Full Stack Developer</p>
+                <p className="selfExp">I build things for the web!</p>
+                <a href="#about"><FontAwesomeIcon icon={faAnglesDown} className="downIcon"/></a>            
             </div>
-        </div>
+        </section>
         
     )
 }
